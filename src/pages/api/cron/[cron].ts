1043f13078016136978bb776e8e8f896c03c0794
simon <simonjzhou@gmail.com>
2023-03-04 22:23:36 -0800

    filter out other languages

--- src/pages/api/cron/[cron].ts
+++ src/pages/api/cron/[cron].ts
@@ -13,12 +13,12 @@ export const config = {
 }
 
 function constructSlackPayload(projects: Project[]) {
-  const projectsPayload = projects.map(({ repo_name, description, stars, language }) => {
+  const projectsPayload = projects.filter(({ language }) => language === "Javascript" || language === "Python").map(({ repo_name, description, stars, language }) => {
     const projects = {
       type: "section",
       text: {
         type: "mrkdwn",
-        text: `*${repo_name}*\nDescription: ${description}\nStars in past 24 hrs: ${stars}\nLink: https://github.com/${repo_name}\nLanguage: ${language}\n\n`
+        text: `*${repo_name}*\n\nDescription: ${description}\nStars in past 24 hrs: ${stars}\nLink: https://github.com/${repo_name}\nLanguage: ${language}\n\n`
       },
     }
 
